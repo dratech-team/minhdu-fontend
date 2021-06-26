3d85883ae926d65cf3eacaf1009b256814d6c939
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-06-26 08:31:48 +0700

    init order

--- apps/sell/src/app/app.module.ts
+++ apps/sell/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OrderComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [
+    OrderComponent
+  ],
 })
 export class AppModule {}
