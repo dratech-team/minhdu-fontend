3c78a71582efbd6c5dd94b193cd6f38cb754adb0
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-06-26 08:45:22 +0700

    init akita

--- apps/sell/src/app/app.module.ts
+++ apps/sell/src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { OrderComponent } from './order/order.component';
+import { AkitaNgEffectsModule } from '@datorama/akita-ng-effects';
+import { environment } from '../environments/environment';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 @NgModule({
   declarations: [AppComponent, OrderComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    environment.production ? [] : AkitaNgDevtools.forRoot({
+      maxAge: 25,
+      logTrace: true
+    }),
+    AkitaNgEffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [
     OrderComponent
-  ],
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
