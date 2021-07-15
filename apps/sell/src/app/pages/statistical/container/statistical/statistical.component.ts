a13ef500ae9183040d754b0803e396e48e39a789
DESKTOP-8IGFEF3\OS <hieu>
2021-07-15 10:45:10 +0700

    biểu đồ

--- apps/sell/src/app/pages/statistical/container/statistical/statistical.component.ts
+++ apps/sell/src/app/pages/statistical/container/statistical/statistical.component.ts
@@ -4,6 +4,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'statistical.component.html'
 })
 export class StatisticalComponent implements OnInit {
+   single = [
+    {
+      "name": "Bình Định",
+      "value": 90
+    },
+    {
+      "name": "Phú Yên",
+      "value": 10
+    },
+    {
+      "name": "Khánh Hòa",
+      "value": 300
+    },
+     {
+       "name": "Quãng Ngãi",
+       "value": 150
+     },
+     {
+       "name": "Đà Nãng",
+       "value": 110
+     },
+
+  ];
+  view:[number, number] = [600, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Tỉnh thành';
+  showYAxisLabel = true;
+  yAxisLabel = 'Số lượng đơn hàng';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
   constructor() {
   }
   ngOnInit() {
--- apps/sell/src/app/pages/statistical/statistical.module.ts
+++ apps/sell/src/app/pages/statistical/statistical.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { StatisticalComponent } from './container/statistical/statistical.component';
 import { StatisticalRoutingModule } from './statistical-routing.module';
 import { CommonModule } from '@angular/common';
+import { BarChartModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   imports: [
     StatisticalRoutingModule,
-    CommonModule
+    CommonModule,
+    BarChartModule
   ],
   declarations: [
     StatisticalComponent,
