aa94a77e678dbeb2095daf864794f63fb008bfd7
DESKTOP-8IGFEF3\OS <hieu>
2021-07-29 15:59:23 +0700

    s

--- apps/sell/src/app/container/sell-layout.component.ts
+++ apps/sell/src/app/container/sell-layout.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { PickMenuComponent } from '../components/pick-menu-mobile/pick-menu.component';
 import { DevelopmentComponent } from '../../../../../libs/components/src/lib/development/development.component';
 import { document } from 'ngx-bootstrap/utils';
+import { login } from '@minhdu-fontend/auth';
 
 
 @Component({
@@ -26,11 +27,15 @@ export class SellLayoutComponent{
   }
 
   changeTab(event: any) {
-   const btnHeader =document.getElementsByClassName('btn-header')
-    for (let i = 0; i < btnHeader.length; i++) {
+
+      const btnHeader = document.getElementsByClassName('btn-header')
+      for (let i = 0; i < btnHeader.length; i++) {
         btnHeader[i].classList.remove('btn-border')
-        
+      }
+    if(event instanceof HTMLImageElement){
+      event.parentElement?.classList.add('btn-border')
+    }else{
+      event.classList.add('btn-border')
     }
-    event.classList.add('btn-border')
   }
 }
