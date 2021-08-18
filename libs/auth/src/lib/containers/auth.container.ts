1a9f795cfe70d24d83f78aa96011c263f1f0000c
DESKTOP-8IGFEF3\OS <hieu>
2021-08-18 19:18:06 +0700

    edit-login

--- libs/auth/src/lib/containers/auth.container.ts
+++ libs/auth/src/lib/containers/auth.container.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthActions, AuthState, selectLoginLoading } from '@minhdu-fontend/auth';
 import { Store } from '@ngrx/store';
+import { App } from '@minhdu-fontend/enums';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +11,7 @@ import { Store } from '@ngrx/store';
 export class AuthComponent implements OnInit {
   loginForm!: FormGroup;
   loading$ = this.store.select(selectLoginLoading);
-
+  appEnum = App;
   constructor(
     private formBuilder: FormBuilder,
     private readonly store: Store<AuthState>
@@ -39,10 +40,9 @@ export class AuthComponent implements OnInit {
     }
     const host = `${window.location.host}`
     console.log(host);
-    const app = host === 'localhost:4000'? 'hr':
-                  host === 'localhost:4001'?'sell':
-                    host === 'localhost:4002'?'warehouse': '';
-    console.log(app);
+    const app = host === 'localhost:4000'? this.appEnum.HR:
+                  host === 'localhost:4001'?this.appEnum.SELL:
+                    host === 'localhost:4002'? this.appEnum.WAREHOUSE: '';
     this.store.dispatch(
       AuthActions.login({
         username: this.f.username.value,
