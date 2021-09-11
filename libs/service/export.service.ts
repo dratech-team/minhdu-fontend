82a0af27d5bc27735eefb282c091a9b41f0f7c84
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-11 13:01:48 +0700

    optimize code

--- libs/service/export.service.ts
+++ libs/service/export.service.ts
@@ -8,16 +8,16 @@ export class ExportService {
 
   print(url: string, params?: any) {
     return this.http
-      .get(url, { params ,observe: 'response', responseType: 'blob' })
+      .get(url, { params, observe: 'response', responseType: 'blob' })
       .subscribe((data) => {
         const link = window.document.createElement('a');
 
-        const fileName = data.headers.get('content-Disposition')
+        const fileName = data.headers.get('content-Disposition');
         const blob = new Blob([data.body as BlobPart], {
-          type: data.body?.type
+          type: data.headers.get('content-Type') as string,
         });
         link.href = window.URL.createObjectURL(blob);
-        link.download = fileName? fileName: 'data'
+        link.download = fileName ? fileName : 'data';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
