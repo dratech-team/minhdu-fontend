8bcb2ad4fa3045f7eb6c183921848f31870f6869
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2021-09-15 20:24:02 +0700

    Fixbug relative 15/09 (#57)
    * s
    
    * s
    
    * s
    
    * s
    
    * s
    
    * s
    
    * s
    
    * s
    
    * s
    
    Co-authored-by: DESKTOP-8IGFEF3\OS <hieu>

--- apps/hr/src/app/pages/employee/components/relative/add-relative.component.ts
+++ apps/hr/src/app/pages/employee/components/relative/add-relative.component.ts
@@ -6,7 +6,6 @@ import { DatePipe } from '@angular/common';
 import { EmployeeAction } from '@minhdu-fontend/employee';
 
 
-
 @Component({
   templateUrl: 'add-relative.component.html'
 })
@@ -26,8 +25,8 @@ export class AddRelativeComponent implements OnInit {
       firstName: [this.data?.relative?.firstName, Validators.required],
       lastName: [this.data?.relative?.lastName, Validators.required],
       issuedBy: [this.data?.relative?.issuedBy, Validators.required],
-      religion: [this.data?.relative?.religion, Validators.required],
-      ethnicity: [this.data?.relative?.ethnicity, Validators.required],
+      religion: [this.data?.relative?.religion],
+      ethnicity: [this.data?.relative?.ethnicity],
 
       birthplace: [
         this.data?.relative?.birthplace, Validators.required],
@@ -44,37 +43,37 @@ export class AddRelativeComponent implements OnInit {
       phone: [this.data?.relative?.phone, Validators.required],
       birthday: [
         this.datePipe.transform(
-          this?.data?.relative?.birthday, 'yyyy-MM-dd'
+          this.data?.relative?.birthday, 'yyyy-MM-dd'
         )
         , Validators.required],
       gender: [this.data?.relative?.gender, Validators.required],
-      note: [this.data?.relative?.note, Validators.required],
+      note: [this.data?.relative?.note],
       relationship: [this.data?.relative?.relationship, Validators.required],
-      career: [this.data?.relative?.career, Validators.required],
-      sos: [this.data?.relative?.sos, Validators.required]
+      career: [this.data?.relative?.career],
+      sos: [this.data?.relative?.sos]
     });
   }
 
   onSubmit() {
     const value = this.formGroup.value;
     const relative = {
-      sos: value.sos,
       relationship: value.relationship,
-      career: value.career,
       firstName: value.firstName,
       lastName: value.lastName,
       gender: value.gender,
       phone: value.phone,
       birthplace: value.birthplace,
-      identify: value.identify.toString(),
+      identify: value.identify?.toString(),
       issuedBy: value.issuedBy,
-      wardId: value.ward === null ? 1 : value.ward,
+      wardId: value.ward,
       address: value.address,
       employeeId: this?.data?.employeeId,
-      birthday: value.birthday ? new Date(value.birthday) : undefined,
-      idCardAt: value.idCardAt ? new Date(value.idCardAt) : undefined,
-      religion: value.religion ? value.religion: undefined,
-      ethnicity: value.ethnicity ? value.ethnicity: undefined,
+      birthday: value.birthday ? value.birthday : undefined,
+      idCardAt: value.idCardAt ? value.idCardAt: undefined,
+      religion: value.religion ? value.religion : undefined,
+      ethnicity: value.ethnicity ? value.ethnicity : undefined,
+      sos: value.sos ? value.sos : undefined,
+      career: value.career ? value.career : undefined
     };
     if (this.data.relative) {
       this.store.dispatch(EmployeeAction.updateRelative(
