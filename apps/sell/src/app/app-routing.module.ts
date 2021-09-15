8f12d764ac9734e4349d05152aa39467bd6af506
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-15 23:54:41 +0700

    fix: path routing

--- apps/sell/src/app/app-routing.module.ts
+++ apps/sell/src/app/app-routing.module.ts
@@ -1,60 +1,76 @@
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SellLayoutComponent } from './container/sell-layout.component';
-import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('@minhdu-fontend/auth').then(m => m.AuthModule)
+    path: 'auth/login',
+    loadChildren: () =>
+      import('@minhdu-fontend/auth').then((m) => m.AuthModule),
   },
   {
-    path: 'ban-hang',
+    path: '',
     component: SellLayoutComponent,
     data: {
-      title: 'Home'
+      title: 'Home',
     },
-    children:[
+    children: [
       {
-        path:'',
-        loadChildren:() => import('./pages/statistical/statistical.module').then(m => m.StatisticalModule)
+        path: '',
+        loadChildren: () =>
+          import('./pages/statistical/statistical.module').then(
+            (m) => m.StatisticalModule
+          ),
       },
       {
         path: 'khach-hang',
-        loadChildren: ()=> import('./pages/customer/customer.module').then(m => m.CustomerModule),
-        data:{
-          title: 'Khách hàng'
-        }
+        loadChildren: () =>
+          import('./pages/customer/customer.module').then(
+            (m) => m.CustomerModule
+          ),
+        data: {
+          title: 'Khách hàng',
+        },
       },
       {
         path: 'hang-hoa',
-        loadChildren: ()=> import('./pages/commodity/commodity.module').then(m => m.CommodityModule),
-        data:{
-          title: 'Hàng hóa'
-        }
+        loadChildren: () =>
+          import('./pages/commodity/commodity.module').then(
+            (m) => m.CommodityModule
+          ),
+        data: {
+          title: 'Hàng hóa',
+        },
       },
       {
         path: 'don-hang',
-        loadChildren: ()=> import('./pages/order/order.module').then(m => m.OrderModule),
-        data:{
-          title: 'Đơn hàng'
-        }
+        loadChildren: () =>
+          import('./pages/order/order.module').then((m) => m.OrderModule),
+        data: {
+          title: 'Đơn hàng',
+        },
       },
       {
-        path:'tuyen-duong',
-        loadChildren:() => import('./pages/route/route.module').then(m => m.RouteModule)
+        path: 'tuyen-duong',
+        loadChildren: () =>
+          import('./pages/route/route.module').then((m) => m.RouteModule),
       },
       {
-        path:'hoa-don',
-        loadChildren:() => import('./pages/bill/bill.module').then(m => m.BillModule)
+        path: 'hoa-don',
+        loadChildren: () =>
+          import('./pages/bill/bill.module').then((m) => m.BillModule),
       },
-      { path: '**', redirectTo: '' }
-    ]
-  }
+      { path: '**', redirectTo: '' },
+    ],
+  },
 ];
 @NgModule({
-  imports:[RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabled' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- apps/warehouse/src/app/app-routing.module.ts
+++ apps/warehouse/src/app/app-routing.module.ts
@@ -1,28 +1,34 @@
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { WarehouseLayoutComponent } from './container/base/warehouse-layout.component';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('@minhdu-fontend/auth').then(m => m.AuthModule)
+    path: 'auth/login',
+    loadChildren: () =>
+      import('@minhdu-fontend/auth').then((m) => m.AuthModule),
   },
   {
-    path:'kho',
-    component:WarehouseLayoutComponent,
-    children:[
+    path: '',
+    component: WarehouseLayoutComponent,
+    children: [
       {
-        path:'',
-        loadChildren:() =>
-          import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+        path: '',
+        loadChildren: () =>
+          import('./pages/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
       },
-    ]
-  }
+    ],
+  },
 ];
 @NgModule({
-  imports:[RouterModule.forRoot(routes, { useHash: true, initialNavigation: 'enabled' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
