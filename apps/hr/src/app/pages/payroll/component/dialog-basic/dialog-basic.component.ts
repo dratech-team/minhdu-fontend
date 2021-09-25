90ad25ddd02d2b666732640125b43d00bdbb6fcc
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-25 22:47:23 +0700

    feat(salary): add model

--- apps/hr/src/app/pages/payroll/component/dialog-basic/dialog-basic.component.ts
+++ apps/hr/src/app/pages/payroll/component/dialog-basic/dialog-basic.component.ts
@@ -1,17 +1,17 @@
+import { DatePipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
-  MAT_DIALOG_DATA,
   MatDialog,
   MatDialogRef,
+  MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
 import { SalaryTypeEnum } from '@minhdu-fontend/enums';
 import { select, Store } from '@ngrx/store';
-import { AppState } from '../../../../reducers';
-import { DatePipe } from '@angular/common';
 import { PayrollAction } from '../../+state/payroll/payroll.action';
-import { selectorAllTemplate } from '../../../template/+state/teamlate-salary-basic/template-basic-salary.selector';
+import { AppState } from '../../../../reducers';
 import { TemplateBasicAction } from '../../../template/+state/teamlate-salary-basic/template-basic-salary.action';
+import { selectorAllTemplate } from '../../../template/+state/teamlate-salary-basic/template-basic-salary.selector';
 
 @Component({
   templateUrl: 'dialog-basic.component.html',
@@ -21,8 +21,14 @@ export class DialogBasicComponent implements OnInit {
   type = SalaryTypeEnum;
   formGroup!: FormGroup;
   submitted = false;
-  checkSalary =  true;
-  templateBasicSalary$ = this.store.pipe(select(selectorAllTemplate))
+  checkSalary = true;
+  templateBasicSalary$ = this.store.pipe(select(selectorAllTemplate));
+  /// FIXME: Dummy data
+  salaries = [
+    { title: 'Lương cơ bản trích BH', type: SalaryTypeEnum.BASIC_INSURANCE },
+    { title: 'Lương theo PL.HD', type: SalaryTypeEnum.BASIC },
+    { title: 'Lương Tín nhiệm', type: SalaryTypeEnum.BASIC },
+  ];
 
   constructor(
     public datePipe: DatePipe,
@@ -34,10 +40,10 @@ export class DialogBasicComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    if(this.data?.salary?.type === this.type.BASIC){
-      this.checkSalary = false
+    if (this.data?.salary?.type === this.type.BASIC) {
+      this.checkSalary = false;
     }
-    this.store.dispatch(TemplateBasicAction.loadALlTemplate())
+    this.store.dispatch(TemplateBasicAction.loadALlTemplate());
     this.formGroup = this.formBuilder.group({
       price: [this.data?.salary?.price, Validators.required],
       type: [
@@ -54,15 +60,14 @@ export class DialogBasicComponent implements OnInit {
 
   onSubmit(): any {
     this.submitted = true;
+
     if (this.formGroup.invalid) {
       return;
     }
     const value = this.formGroup.value;
+    console.log('value salary', value);
     const salary = {
-      title:
-        value.type.trim() === this.type.BASIC_INSURANCE
-          ? 'Lương cơ bản trích BH'
-          : 'Lương theo PL.HĐ',
+      title: value.title,
       price: this.checkSalary
         ? typeof value.price === 'string'
           ? Number(value.price.replace(this.numberChars, ''))
@@ -81,10 +86,12 @@ export class DialogBasicComponent implements OnInit {
         })
       );
     } else {
-      this.store.dispatch(PayrollAction.addSalary({
-        payrollId: this.data.payroll.id,
-        salary: salary
-      }))
+      this.store.dispatch(
+        PayrollAction.addSalary({
+          payrollId: this.data.payroll.id,
+          salary: salary,
+        })
+      );
     }
     this.dialogRef.close();
   }
