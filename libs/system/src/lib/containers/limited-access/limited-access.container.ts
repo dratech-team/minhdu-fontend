1c9f3a8278ca3e74c0aee3ac02225f8725a81cca
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2021-10-18 18:08:15 +0700

    page-403 (#373)
    * page-403
    
    * s
    
    Co-authored-by: DESKTOP-8IGFEF3\OS <hieu>

---
+++ libs/system/src/lib/containers/limited-access/limited-access.container.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'limited-access.container.html',
+  styleUrls: ['limited-access.scss']
+})
+export class LimitedAccessContainer {
+}
