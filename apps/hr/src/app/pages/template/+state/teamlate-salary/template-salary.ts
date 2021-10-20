1fd7ce136eee85098163ed9ac483f3496e491d0b
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-20 21:03:20 +0700

    no message

--- apps/hr/src/app/pages/template/+state/teamlate-salary/template-salary.ts
+++ apps/hr/src/app/pages/template/+state/teamlate-salary/template-salary.ts
@@ -4,9 +4,9 @@ import { PartialDayEnum } from '@minhdu-fontend/data-models';
 export interface TemplateSalary {
   id: number,
   title: string,
-  price?: number,
+  price: number,
   type: SalaryTypeEnum,
-  positionId?: any,
-  partialDay?: PartialDayEnum,
-  unit?: DatetimeUnitEnum
+  positionId: any,
+  partialDay: PartialDayEnum,
+  unit: DatetimeUnitEnum
 }
