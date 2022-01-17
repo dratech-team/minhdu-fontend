974d5bf8767e4ebe3aac3875b8c58541ac92d591
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-01-17 12:08:56 +0700

    refactor(context-menu): change name func

--- libs/components/src/lib/mouse-right/mouse-right.component.ts
+++ libs/components/src/lib/mouse-right/mouse-right.component.ts
@@ -5,85 +5,84 @@ import { ItemContextMenu } from '../../../../enums/sell/page-type.enum';
 @Component({
   selector: 'app-mouse-right',
   templateUrl: './mouse-right.component.html',
-  styleUrls: ['./mouse-right.component.scss']
+  styleUrls: ['./mouse-right.component.scss'],
 })
 export class MouseRightComponent {
   @Input() items?: ItemContextMenu[];
   ItemContextMenu = ItemContextMenu;
-  @Output() addEvent = new EventEmitter();
-  @Output() deleteEvent = new EventEmitter();
-  @Output() permanentlyDeletedEvent = new EventEmitter();
-  @Output() readEvent = new EventEmitter();
-  @Output() payment = new EventEmitter();
-  @Output() delivered = new EventEmitter();
-  @Output() restore = new EventEmitter();
-  @Output() HistoryPayroll = new EventEmitter();
-  @Output() Employee = new EventEmitter();
-  @Output() payroll = new EventEmitter();
-  @Output() overtime = new EventEmitter();
-  @Output() listPosition = new EventEmitter();
-  @Output() update = new EventEmitter();
+  @Output() onAdd = new EventEmitter();
+  @Output() onDel = new EventEmitter();
+  @Output() onDelPerm = new EventEmitter();
+  @Output() onDetail = new EventEmitter();
+  @Output() onPay = new EventEmitter();
+  @Output() onEnd = new EventEmitter();
+  @Output() onRestore = new EventEmitter();
+  @Output() onHisPayroll = new EventEmitter();
+  @Output() onProfile = new EventEmitter();
+  @Output() onPayroll = new EventEmitter();
+  @Output() onOvertime = new EventEmitter();
+  @Output() onPositions = new EventEmitter();
+  @Output() onUpdate = new EventEmitter();
 
-  constructor(
-    private contextMenuService: ContextMenuService
-  ) {
-  }
+  constructor(private contextMenuService: ContextMenuService) {}
 
   public onContextMenu($event: MouseEvent, item: any): void {
     this.contextMenuService.show.next({
       event: $event,
-      item: item
+      item: item,
     });
     $event.preventDefault();
     $event.stopPropagation();
   }
 
   add(item: any): void {
-    this.addEvent.emit(item);
+    this.onAdd.emit(item);
   }
 
   delete(item: any): void {
-    this.deleteEvent.emit(item);
+    this.onDel.emit(item);
+  }
+  
+  update(item: any) {
+    this.onUpdate.emit(item);
   }
 
   permanentlyDeleted(item: any): void {
-    this.permanentlyDeletedEvent.emit(item);
+    this.onDelPerm.emit(item);
   }
 
   readAndUpdate(item: any): void {
-    this.readEvent.emit(item);
+    this.onDetail.emit(item);
   }
 
   onPayment(item: any): void {
-    this.payment.emit(item);
+    this.onPay.emit(item);
   }
 
   onDelivered(item: any) {
-    this.delivered.emit(item);
+    this.onEnd.emit(item);
   }
 
-  onRestore(item: any) {
-    this.restore.emit(item);
+  restore(item: any) {
+    this.onRestore.emit(item);
   }
   onHistoryPayroll(item: any) {
-    this.HistoryPayroll.emit(item);
-  }
-  onEmployee(item: any) {
-    this.Employee.emit(item);
+    this.onHisPayroll.emit(item);
   }
 
-  onPayroll(item: any) {
-    this.payroll.emit(item);
+  onEmployee(item: any) {
+    this.onProfile.emit(item);
   }
-  onOvertime(item: any) {
-    this.overtime.emit(item);
+
+  payroll(item: any) {
+    this.onPayroll.emit(item);
+  }
+  
+  overtime(item: any) {
+    this.onOvertime.emit(item);
   }
 
   onListPosition(item: any) {
-    this.listPosition.emit(item);
-  }
-
-  onUpdate(item: any) {
-    this.update.emit(item);
+    this.onPositions.emit(item);
   }
 }
