6f49c1768ec2e6b528492f06479f89b213f17c41
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-01-29 11:08:38 +0700

    a

--- apps/warehouse/src/app/pages/dashboard/dashboard.component.ts
+++ apps/warehouse/src/app/pages/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import { Store } from '@ngrx/store';
 import { MainAction } from '../../states/main.action';
 import { selectedWareHouse } from '../../states/main.selector';
 import { getSelectors } from '../../../../../../libs/utils/getState.ultils';
+import { MatDialog } from '@angular/material/dialog';
+import { MedicineDialogComponent } from '../../container/dashboard/medicine/components/medicine-dialog/medicine-dialog.component';
 
 @Component({
   selector: 'app-main-dashboard',
@@ -18,18 +20,19 @@ export class DashboardComponent implements OnInit {
   warehouseType = getSelectors(selectedWareHouse, this.store);
 
   constructor(
-    private readonly store: Store
+    private readonly store: Store,
+    private readonly dialog: MatDialog
   ) {
   }
 
   ngOnInit() {
     this.selectWarehouse.valueChanges.subscribe(value => {
-      this.store.dispatch(MainAction.updateState({warehouse: value}));
+      this.store.dispatch(MainAction.updateState({ warehouse: value }));
       this.warehouseType = value;
     });
   }
 
   import() {
-
+    this.dialog.open(MedicineDialogComponent);
   }
 }
