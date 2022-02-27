4ec67090a1f7fee5a3a9174de4cd6b0a2c53bf7f
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-02-27 21:10:13 +0700

    done

---
+++ apps/sell/src/app/pages/order/service/order-history.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {BaseService} from '@minhdu-fontend/service';
+import {OrderHistory} from '../+state/order.interface';
+import {HttpClient} from '@angular/common/http';
+import {Api} from '@minhdu-fontend/constants';
+import {Observable} from 'rxjs';
+import {ResponsePaginate} from "@minhdu-fontend/data-models";
+
+@Injectable({providedIn: 'root'})
+export class OrderHistoryService extends BaseService<OrderHistory> {
+  constructor(
+    public readonly http: HttpClient
+  ) {
+    super(Api.SELL.ORDER.ORDER_HISTORY, http);
+  }
+
+  pagination(params?: any): Observable<ResponsePaginate<OrderHistory>> {
+    return super.pagination(params);
+  }
+}
