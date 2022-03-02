7f69843c4a3bab14b7f91a620de9a1731b1796dd
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-03-02 16:53:10 +0700

    done

--- apps/sell/src/app/app.module.ts
+++ apps/sell/src/app/app.module.ts
@@ -27,8 +27,18 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { AuthEffects } from '../../../../libs/auth/src/lib/+state/auth.effects';
 import { MatTabsModule } from '@angular/material/tabs';
 import localeVi from '@angular/common/locales/vi';
+import {NZ_ICONS} from "ng-zorro-antd/icon";
+import * as AllIcons from "@ant-design/icons-angular/icons";
+import {IconDefinition} from "@ant-design/icons-angular";
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+
 
 registerLocaleData(localeVi);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   imports: [
@@ -74,7 +84,9 @@ registerLocaleData(localeVi);
     },
     HashLocationStrategy,
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
-    { provide: LOCALE_ID, useValue: 'vi-VN' }
+    { provide: LOCALE_ID, useValue: 'vi-VN' },
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: NZ_ICONS, useValue: icons }
   ],
   bootstrap: [AppComponent]
 })
