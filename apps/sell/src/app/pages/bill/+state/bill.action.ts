ed5bde075b0ed86feb0d56e50665b58636715de6
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-03-05 13:58:26 +0700

    a

--- apps/sell/src/app/pages/bill/+state/bill.action.ts
+++ apps/sell/src/app/pages/bill/+state/bill.action.ts
@@ -2,25 +2,26 @@ import { createAction, props } from '@ngrx/store';
 import { Bill } from './Bill.interface';
 
 
-export const loadInit = createAction(
+const loadInit = createAction(
   '[LOAD_BILLS] Load Bill',
   props<{ take: number, skip: number }>()
 );
 
-export const loadInitSuccess = createAction(
+const loadInitSuccess = createAction(
   '[LOAD_BILLS] Load Bill Success',
   props<{ bills: Bill[] }>()
 );
 
-export const loadMoreBills = createAction(
+const loadMoreBills = createAction(
   '[LOAD_MORE_BILLS] Load More Bill',
   props<{ take: number, skip: number }>()
 );
 
-export const loadMoreBillsSuccess = createAction(
+const loadMoreBillsSuccess = createAction(
   '[LOAD_MORE_BILLS] Load More Bill Success',
   props<{ bills: Bill[] }>()
 );
+
 export const BillAction = {
   loadInit,
   loadInitSuccess,
