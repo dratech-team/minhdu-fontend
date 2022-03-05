6851bf8c50d2fb22d5e58e2c26771a50d2f9e275
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-03-05 13:58:00 +0700

    format

--- apps/sell/src/app/pages/bill/+state/bill.action.ts
+++ apps/sell/src/app/pages/bill/+state/bill.action.ts
@@ -4,26 +4,26 @@ import { Bill } from './Bill.interface';
 
 export const loadInit = createAction(
   '[LOAD_BILLS] Load Bill',
-  props<{take:number, skip: number }>()
-)
+  props<{ take: number, skip: number }>()
+);
 
 export const loadInitSuccess = createAction(
   '[LOAD_BILLS] Load Bill Success',
-  props<{bills:Bill[]}>()
-)
+  props<{ bills: Bill[] }>()
+);
 
 export const loadMoreBills = createAction(
   '[LOAD_MORE_BILLS] Load More Bill',
-  props<{take:number, skip: number }>()
-)
+  props<{ take: number, skip: number }>()
+);
 
 export const loadMoreBillsSuccess = createAction(
   '[LOAD_MORE_BILLS] Load More Bill Success',
-  props<{bills:Bill[]}>()
-)
-export const BillAction ={
+  props<{ bills: Bill[] }>()
+);
+export const BillAction = {
   loadInit,
   loadInitSuccess,
   loadMoreBills,
-  loadMoreBillsSuccess,
-}
+  loadMoreBillsSuccess
+};
--- apps/sell/src/app/pages/bill/+state/bill.reducer.ts
+++ apps/sell/src/app/pages/bill/+state/bill.reducer.ts
@@ -3,19 +3,20 @@ import { Bill } from './Bill.interface';
 import { createReducer, on } from '@ngrx/store';
 import { BillAction } from './bill.action';
 
-export interface BillState extends EntityState<Bill>{
+export interface BillState extends EntityState<Bill> {
   loaded: boolean,
   selectedBillId: number,
 }
+
 export const adapter: EntityAdapter<Bill> = createEntityAdapter<Bill>();
-export const initialBill = adapter.getInitialState({loaded: false});
+export const initialBill = adapter.getInitialState({ loaded: false });
 export const BillReducer = createReducer(
   initialBill,
-  on(BillAction.loadInitSuccess, (state,action)=>
-  adapter.setAll(action.bills, {...state, loaded: true})
+  on(BillAction.loadInitSuccess, (state, action) =>
+    adapter.setAll(action.bills, { ...state, loaded: true })
   ),
-  on(BillAction.loadMoreBillsSuccess, (state,action)=>
-    adapter.addMany(action.bills, {...state, loaded: true})
+  on(BillAction.loadMoreBillsSuccess, (state, action) =>
+    adapter.addMany(action.bills, { ...state, loaded: true })
   )
-)
-export const {selectAll, selectEntities} = adapter.getSelectors()
+);
+export const { selectAll, selectEntities } = adapter.getSelectors();
