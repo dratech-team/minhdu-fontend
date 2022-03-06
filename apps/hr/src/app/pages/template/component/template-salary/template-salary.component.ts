23bb354eeef674fc37f812ef137f3976248fae0f
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-03-06 12:57:24 +0700

    update tamplate

--- apps/hr/src/app/pages/template/component/template-salary/template-salary.component.ts
+++ apps/hr/src/app/pages/template/component/template-salary/template-salary.component.ts
@@ -1,11 +1,17 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { select, Store } from '@ngrx/store';
-import { SalaryTypeEnum } from '@minhdu-fontend/enums';
-import { BlockSalariesConstant } from '@minhdu-fontend/constants';
-import { TemplateSalaryAction } from '../../+state/teamlate-salary/template-salary.action';
-import { selectTemplateAdded, selectTemplateLoaded } from '../../+state/teamlate-salary/template-salary.selector';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {select, Store} from '@ngrx/store';
+import {SalaryTypeEnum} from '@minhdu-fontend/enums';
+import {BlockSalariesConstant} from '@minhdu-fontend/constants';
+import {TemplateSalaryAction} from '../../+state/teamlate-salary/template-salary.action';
+import {selectTemplateAdded, selectTemplateLoaded} from '../../+state/teamlate-salary/template-salary.selector';
+import {getAllOrgchart, OrgchartActions} from "@minhdu-fontend/orgchart";
+import {Branch} from "@minhdu-fontend/data-models";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import * as lodash from "lodash";
+import {searchAutocomplete} from "@minhdu-fontend/utils";
+import {startWith} from "rxjs/operators";
 
 
 @Component({
@@ -17,17 +23,25 @@ export class TemplateSalaryComponent implements OnInit {
   submitted = false;
   type = SalaryTypeEnum;
   blockSalary = BlockSalariesConstant;
+  branches = new FormControl();
+  branches$ = this.store.pipe(select(getAllOrgchart));
+  branchesSelected: Branch[] = []
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private readonly formBuilder: FormBuilder,
     private readonly store: Store,
+    private readonly snackbar: MatSnackBar,
     private readonly dialogRef: MatDialogRef<TemplateSalaryComponent>
   ) {
   }
 
   ngOnInit() {
+    this.store.dispatch(OrgchartActions.init());
     if (this.data?.isUpdate) {
+      if (this.data.template?.branches) {
+        this.branchesSelected = [...this.data.template?.branches]
+      }
       this.formGroup = this.formBuilder.group({
         type: [this.data.template.type, Validators.required],
         price: [this.data.template.price],
@@ -40,6 +54,10 @@ export class TemplateSalaryComponent implements OnInit {
         title: ['Lương cơ bản trích BH']
       });
     }
+    this.branches$ = searchAutocomplete(
+      this.branches.valueChanges.pipe(startWith(this.data?.template?.branch?.name || '')),
+      this.branches$
+    );
   }
 
   get f() {
@@ -51,11 +69,15 @@ export class TemplateSalaryComponent implements OnInit {
     if (this.formGroup.invalid) {
       return;
     }
+    if (this.branches.value) {
+      return this.snackbar.open('Đơn vị phải chọn không được nhập', '', {duration: 1500})
+    }
     const value = this.formGroup.value;
     const template = {
       title: value.title,
       price: typeof (value.price) === 'string' ? Number(value.price.replace(this.numberChars, '')) : value.price,
-      type: value.type
+      type: value.type,
+      branchIds: this.branchesSelected.map(val => val.id)
     };
     if (this.data) {
       this.store.dispatch(TemplateSalaryAction.updateTemplate(
@@ -65,7 +87,7 @@ export class TemplateSalaryComponent implements OnInit {
         }));
     } else {
       this.store.dispatch(TemplateSalaryAction.AddTemplate(
-        { template: template }));
+        {template: template}));
     }
     this.store.pipe(select(selectTemplateAdded)).subscribe(added => {
       if (added) {
@@ -73,4 +95,25 @@ export class TemplateSalaryComponent implements OnInit {
       }
     });
   }
+
+  onSelectBranch(event: any, branch: Branch, branchesInput: HTMLInputElement) {
+    if (event.isUserInput) {
+      if (branch.id) {
+        if (this.branchesSelected.some(item => item.id === branch.id)) {
+          this.snackbar.open('Đơn vị đã được chọn', '', {duration: 1000});
+        } else {
+          this.branchesSelected.push(branch);
+        }
+      }
+      setTimeout(() => {
+        this.branches.setValue('');
+        branchesInput.blur();
+      });
+    }
+  }
+
+  removeBranchSelected(branch: Branch) {
+    lodash.remove(this.branchesSelected, branch)
+  }
+
 }
