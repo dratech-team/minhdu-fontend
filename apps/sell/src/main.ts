f31f8521a13d42aa4d56998628c1cdf29ffcc5cd
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-03-15 23:45:52 +0700

    init state ui order

--- apps/sell/src/main.ts
+++ apps/sell/src/main.ts
@@ -3,15 +3,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { persistState } from '@datorama/akita';
+import { persistState, PersistStateSelectFn } from '@datorama/akita';
 import { StorageName } from './app/shared/constaints/storage-name.const';
+import { OrderState } from './app/pages/order/+state/order.store';
 
 if (environment.production) {
   enableProdMode();
 }
 
+const selectOrder: PersistStateSelectFn<OrderState> = (state) => ({ token: state.token });
+selectOrder.storeName = StorageName.ORDER;
+
 const storage = persistState({
-  include: [StorageName.APP]
+  include: [StorageName.APP],
+  select: [selectOrder]
 });
 
 const providers = [{ provide: 'persistStorage', useValue: storage }];
