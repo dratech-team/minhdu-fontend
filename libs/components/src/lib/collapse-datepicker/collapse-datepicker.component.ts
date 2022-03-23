8310cb99ee1d81072253410be3abdca62c181bef
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-03-23 14:17:46 +0700

    init  startAt and endedAt  in order

--- libs/components/src/lib/collapse-datepicker/collapse-datepicker.component.ts
+++ libs/components/src/lib/collapse-datepicker/collapse-datepicker.component.ts
@@ -1,22 +1,33 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl} from "@angular/forms";
+import {getFirstDayInMonth, getLastDayInMonth} from "@minhdu-fontend/utils";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'minhdu-fontend-collapse-datepicker',
   templateUrl: 'collapse-datepicker.component.html'
 })
-export class CollapseDatepickerComponent implements  OnInit{
+export class CollapseDatepickerComponent implements OnInit {
   @Input() title: string = '';
+  @Input() endedAtInit: Date = getFirstDayInMonth(new Date());
+  @Input() startedAtInit: Date = getLastDayInMonth(new Date());
   @Output() onPicker = new EventEmitter<any>();
   formTitlePicker = new FormControl();
   formRange = new FormControl();
   formRadio = new FormControl()
   visible = false
+
+  constructor(
+    private readonly datePipe: DatePipe
+  ) {
+  }
+
   ngOnInit() {
+    console.log(getFirstDayInMonth(new Date()))
     this.formRange.valueChanges.subscribe(val => {
       this.formRadio.setValue(1)
       this.onPicker.emit({
-        startedAt: val[0], endedAt: val[1]
+        start: val[0], end: val[1]
       });
     })
   }
@@ -25,8 +36,8 @@ export class CollapseDatepickerComponent implements  OnInit{
     this.visible = false
     this.formTitlePicker.setValue($event.titleDatepicker.title)
     this.onPicker.emit({
-      startedAt: $event.titleDatepicker.startedAt,
-      endedAt: $event.titleDatepicker.endedAt
+      start: $event.titleDatepicker.startedAt,
+      end: $event.titleDatepicker.endedAt
     });
   }
 }
