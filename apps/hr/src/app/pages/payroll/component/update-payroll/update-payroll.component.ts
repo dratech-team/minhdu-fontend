cb2a8df4fc9a565f51a71f69699bdfe1f24a92c1
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-03-23 11:33:12 +0700

    update-payroll

---
+++ apps/hr/src/app/pages/payroll/component/update-payroll/update-payroll.component.ts
@@ -0,0 +1,84 @@
+import {Component, Inject, OnInit} from "@angular/core";
+import {DatePipe} from "@angular/common";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {select, Store} from "@ngrx/store";
+import {getAllOrgchart, OrgchartActions} from "@minhdu-fontend/orgchart";
+import {Branch, Position} from "@minhdu-fontend/data-models";
+import {map} from "rxjs/operators";
+import {selectedAddedPayroll} from "../../+state/payroll/payroll.selector";
+import {RecipeTypesConstant} from "@minhdu-fontend/constants";
+import {PayrollAction} from "../../+state/payroll/payroll.action";
+
+@Component({
+  templateUrl: 'update-payroll.component.html'
+})
+export class UpdatePayrollComponent implements OnInit {
+  formGroup!: FormGroup
+  positions?: Position[];
+  recipeTypeConstant = RecipeTypesConstant
+  branches$ = this.store.pipe(select(getAllOrgchart)).pipe(map(branches => {
+    if (branches.length === 1) {
+      this.formGroup.get('branch')?.setValue(branches[0])
+      this.positions = branches[0].positions
+    } else {
+      this.positions = branches.find(branch => branch.name === this.data.payroll.branch)?.positions
+    }
+    return branches
+  }));
+  compareFN = (o1: any, o2: any) => (typeof o1 === 'string' && o2 ? o1 == o2.name : o1.id === o2.id);
+  compareRecipe = (o1: any, o2: any) => (typeof o1 === 'string' && o2 ? o1 == o2.value : o1.value === o2.value);
+
+  constructor(
+    private readonly datePipe: DatePipe,
+    private readonly store: Store,
+    private readonly formBuilder: FormBuilder,
+    private readonly dialogRef: MatDialogRef<UpdatePayrollComponent>,
+    @Inject(MAT_DIALOG_DATA) public data?: any
+  ) {
+  }
+
+  ngOnInit() {
+    this.store.dispatch(OrgchartActions.init())
+    this.formGroup = this.formBuilder.group({
+      createdAt: [this.datePipe.transform(this.data.payroll.createdAt, 'yyyy-MM-dd'), Validators.required],
+      branch: [this.data.payroll.branch, Validators.required],
+      position: [this.data.payroll.position, Validators.required],
+      workday: [this.data.payroll.workday, Validators.required],
+      recipeType: [this.data.payroll.recipeType, Validators.required]
+    })
+    this.formGroup.get('branch')?.valueChanges.subscribe(val => {
+      this.formGroup.get('position')?.setValue('')
+      this.positions = val.positions
+    })
+  }
+
+  onSubmit(): any {
+    if (this.formGroup.invalid) {
+      return
+    }
+    const value = this.formGroup.value
+    const payroll = {
+      workday: value.workday,
+      createdAt: value.createdAt,
+    }
+    if (value.position?.id) {
+      Object.assign(payroll, {positionId: value.position.id,})
+    }
+    if (value.branch?.id) {
+      Object.assign(payroll, {branchId: value.branch.id,})
+    }
+    if (value.recipeType?.value) {
+      Object.assign(payroll, {recipeType: value.recipeType.value})
+    }
+    this.store.dispatch(PayrollAction.updatePayroll({
+      id: this.data.payroll.id,
+      payroll: payroll
+    }))
+    this.store.select(selectedAddedPayroll).subscribe(added => {
+      if (added) {
+        this.dialogRef.close()
+      }
+    })
+  }
+}
