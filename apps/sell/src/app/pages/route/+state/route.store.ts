5159ece9a8b8733679da9331a99ce11ba8ff37d0
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-03-24 14:53:44 +0700

    format-code

--- apps/sell/src/app/pages/route/+state/route.store.ts
+++ apps/sell/src/app/pages/route/+state/route.store.ts
@@ -2,15 +2,36 @@ import {EntityState, EntityStore, StoreConfig} from '@datorama/akita';
 import {Injectable} from '@angular/core';
 import {RouteEntity} from '../entities/route.entity';
 import {StorageName} from '../../../shared/constaints/storage-name.const';
+import {SearchRouteEntity} from "../entities/search-route.entity";
+import {RangeDay} from "@minhdu-fontend/data-models";
+import {getFirstDayInMonth, getLastDayInMonth} from "@minhdu-fontend/utils";
 
 export interface RouteState extends EntityState<RouteEntity> {
   loading: boolean;
   added: boolean | null,
   total: number,
-  expandedAll: boolean
+  expandedAll: boolean,
+  search: SearchRouteEntity
 }
 
-export const createInitialState = () => ({loading: true, added: null, total: 0, expandedAll: false});
+export const createInitialState = () => ({
+  loading: true,
+  added: null,
+  total: 0,
+  expandedAll: false,
+  search: {
+    search: '',
+    startedAt: {
+      start: getFirstDayInMonth(new Date()),
+      end: getLastDayInMonth(new Date())
+    },
+    endedAt: {
+      start: getFirstDayInMonth(new Date()),
+      end: getLastDayInMonth(new Date())
+    },
+    status: -1
+  }
+});
 
 @Injectable({providedIn: 'root'})
 @StoreConfig({name: StorageName.ROUTE})
