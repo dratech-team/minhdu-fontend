aa1462d6a4bd472df49a51baf6f10d32ba47c4b3
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-03-26 13:38:23 +0700

    feat(middleware): init basse

---
+++ apps/sell/src/app/shared/middlewares/base.middleware.ts
@@ -0,0 +1,36 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MethodHttp } from '../http/method.http';
+
+export class BaseMiddleware<L, A, U> implements HttpInterceptor {
+  intercept(req: HttpRequest<L | A | U>, next: HttpHandler): Observable<HttpEvent<L | A | U>> {
+    switch (req.method) {
+      case MethodHttp.GET: {
+        // this.getRequest(req.params.);
+        break;
+      }
+      case MethodHttp.POST: {
+        break;
+      }
+      case MethodHttp.PATCH: {
+        break;
+      }
+      case MethodHttp.DELETE: {
+        break;
+      }
+    }
+    return next.handle(req);
+  }
+
+  load(param: L) {
+
+  }
+
+  update(param: U) {
+
+  }
+
+  add(param: A) {
+
+  }
+}
