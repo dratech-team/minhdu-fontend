1de252ca6b811036d46b515bba7d97e1893f5a9d
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-03-30 17:12:43 +0700

    fix codition

--- libs/utils/handle-salary-payroll.ts
+++ libs/utils/handle-salary-payroll.ts
@@ -5,22 +5,15 @@ import { SalaryTypeEnum } from '../enums';
 
 export const filterSalaryPayroll = (salaryPayrolls: SalaryPayroll[], salary: Salary) => {
   return salaryPayrolls.filter(salaryPayroll => {
-    console.log(moment(salaryPayroll.salary.datetime).isSame(salary.datetime, 'day'));
-    return (salaryPayroll.salary.title === salaryPayroll.salary.title
-      && (
-        (salary.type === SalaryTypeEnum.BASIC || salaryPayroll.salary.type === SalaryTypeEnum.STAY) ?
-          moment(salaryPayroll.salary.datetime).isSame(salary.datetime, 'month') :
-          moment(salaryPayroll.salary.datetime).isSame(salary.datetime, 'day')
-      )
-      && (salaryPayroll.salary.price ?
-        salaryPayroll.salary.price === salaryPayroll.salary.price : true)
-      && (salaryPayroll.salary?.allowance ?
-        (salaryPayroll.salary?.allowance?.price === salaryPayroll.salary.allowance.price
-          && salaryPayroll.salary?.allowance?.title === salaryPayroll.salary.allowance.title
-        ) : true)
-    );
+    return salaryPayroll.salary.title === salary.title
+      && isSame(salaryPayroll.salary.datetime, salary.datetime, (salary.type === SalaryTypeEnum.BASIC || salaryPayroll.salary.type === SalaryTypeEnum.STAY) ? 'month' : 'day')
+      && salaryPayroll.salary?.price === salary.price
+      && salaryPayroll.salary?.allowance?.price === salary?.allowance?.price
+      && salaryPayroll.salary?.allowance?.title === salary?.allowance?.title;
   });
 };
+
+const isSame = (date1: Date, date2: Date, type: 'day' | 'month') => moment(date1).isSame(date2, type);
 
 export const updateSelectOneSalaryPayroll = (event: boolean, salaryPayroll: SalaryPayroll, salaryPayrolls: SalaryPayroll[]) => {
   if (event) {
