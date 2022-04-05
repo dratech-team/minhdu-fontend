f6341466d7fddef2bd8a563ebed8b86edf6adf3a
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-05 10:00:49 +0700

    UI import  app warehouse (#840)
    * ui-import-commodity
    
    * ui-import

--- apps/warehouse/src/app/pages/migrate/migrate.module.ts
+++ apps/warehouse/src/app/pages/migrate/migrate.module.ts
@@ -4,14 +4,21 @@ import { MigrateService } from './services/migrate.service';
 import { CommonModule } from '@angular/common';
 import { MigrateRoutingModule } from './migrate-routing.module';
 import { MigrateComponent } from './containers';
+import {ImportCommodityComponent} from "./components/import-commodity/import-commodity.component";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ComponentsModule} from "@minhdu-fontend/components";
 
 @NgModule({
   imports: [
     CommonModule,
     NzTableModule,
-    MigrateRoutingModule
+    MigrateRoutingModule,
+    NzCollapseModule,
+    ReactiveFormsModule,
+    ComponentsModule
   ],
-  declarations: [MigrateComponent],
+  declarations: [MigrateComponent,ImportCommodityComponent],
   providers: [MigrateService],
   exports: [MigrateComponent]
 })
