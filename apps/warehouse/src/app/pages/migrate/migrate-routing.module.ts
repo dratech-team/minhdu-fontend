56594abf5bad0d6878d63289f6799861d0e0ba38
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-05 15:26:31 +0700

    fix

--- apps/warehouse/src/app/pages/migrate/migrate-routing.module.ts
+++ apps/warehouse/src/app/pages/migrate/migrate-routing.module.ts
@@ -1,11 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { MigrateComponent } from './containers';
+import {MigrateComponent} from './containers';
+import {ImportCommodityComponent} from "./components/import-commodity/import-commodity.component";
+import {ExportCommodityComponent} from "./components/export-commodity/export-commodity.component";
 
 const routes: Routes = [
   {
     path: '',
     component: MigrateComponent,
+    children:[
+      {
+        path: '',
+        component: ImportCommodityComponent
+      },
+      {
+        path: 'xuat-kho',
+        component: ExportCommodityComponent
+      },
+    ]
   }
 ];
 
