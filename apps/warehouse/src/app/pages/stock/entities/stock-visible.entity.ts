241ca7718ba7dd2577b520933c8ecb493c2bae24
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-07 19:50:25 +0700

    fix-ui-stock

--- apps/warehouse/src/app/pages/stock/entities/stock-visible.entity.ts
+++ apps/warehouse/src/app/pages/stock/entities/stock-visible.entity.ts
@@ -2,16 +2,23 @@ import {VisibleEntity} from "@minhdu-fontend/data-models";
 
 export interface StockVisibleEntity {
   readonly stt: VisibleEntity;
-  readonly code: VisibleEntity;
-  readonly name: VisibleEntity;
-  readonly warehouseType: VisibleEntity;
   readonly price: VisibleEntity;
   readonly amount: VisibleEntity;
-  readonly totalCash: VisibleEntity;
-  readonly barcode: VisibleEntity;
-  readonly provider: VisibleEntity;
   readonly discount: VisibleEntity;
-  readonly exp: VisibleEntity;
-  readonly unit: VisibleEntity;
   readonly createdAt: VisibleEntity;
+  readonly type: VisibleEntity
+  readonly accountedAt: VisibleEntity,
+  readonly billedAt: VisibleEntity,
+  readonly billCode: VisibleEntity,
+  readonly discountType: VisibleEntity;
+  readonly orderedAt: VisibleEntity
+  readonly importedAt: VisibleEntity
+  readonly completedAt: VisibleEntity
+  readonly approvedAt: VisibleEntity
+  readonly note: VisibleEntity
+  readonly attachment: VisibleEntity
+  readonly product: VisibleEntity;
+  readonly consignment: VisibleEntity
+  readonly branch: VisibleEntity;
+  readonly tax: VisibleEntity;
 }
