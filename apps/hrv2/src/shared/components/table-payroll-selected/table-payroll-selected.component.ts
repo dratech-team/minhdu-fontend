e17fb4b0a828be47a56555a27456e56c70cbfe08
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-12 17:28:32 +0700

    s

---
+++ apps/hrv2/src/shared/components/table-payroll-selected/table-payroll-selected.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector:'minhdu-fontend-table-payroll-selected',
+  templateUrl: 'table-payroll-selected.component.html'
+})
+export class TablePayrollSelectedComponent implements OnInit{
+  @Input() formGroup!: FormGroup
+  constructor() {
+  }
+  ngOnInit() {
+    this.formGroup.get('payrollIds')!.valueChanges.subscribe(idss => {
+
+    })
+  }
+}
