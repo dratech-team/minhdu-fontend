59ca5d83d3732f9d9058de99bcf42fdf67f14471
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-15 10:25:33 +0700

    s

---
+++ apps/hrv2/src/app/pages/setting/rank.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RankRoutingModule} from "./rank-routing.module";
+import {RankComponent} from "./containers/rannk/rank.component";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ComponentsModule} from "@minhdu-fontend/components";
+import {NzDropDownModule} from "ng-zorro-antd/dropdown";
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {SharedModule} from "../../../shared/shared.module";
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CommonModule,
+    RankRoutingModule,
+    NzTableModule,
+    NzCollapseModule,
+    ReactiveFormsModule,
+    ComponentsModule,
+    NzDropDownModule,
+    NzIconModule,
+  ],
+  declarations:[RankComponent],
+})
+
+export class RankModule{
+
+}
