cf296fad230a90bc841c28409fb549bc8af4f73a
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-15 10:32:58 +0700

    s

--- apps/hrv2/src/app/pages/setting/salary/containers/setting-salary/setting-salary.component.ts
+++ apps/hrv2/src/app/pages/setting/salary/containers/setting-salary/setting-salary.component.ts
@@ -1,36 +1,130 @@
-import {Component, OnInit} from "@angular/core";
-import {FormControl, FormGroup} from "@angular/forms";
-import {Observable} from "rxjs";
-import {Branch, Position} from "@minhdu-fontend/data-models";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {debounceTime, map} from 'rxjs/operators';
+import {PaginationDto} from '@minhdu-fontend/constants';
+import {SettingSalaryActions} from '../../state/setting-salary.actions';
+import {SettingSalaryQuery} from '../../state/setting-salary.query';
+import {Actions} from '@datorama/akita-ng-effects';
+import {NzModalService} from 'ng-zorro-antd/modal';
+import {SettingSalaryStore} from '../../state/setting-salary.store';
+import {SalaryTypeEnum} from "../../enums";
+import {blockSalariesConstant} from "../../constants";
+import {SettingSalaryDialogComponent} from "../../components";
+import {SettingSalaryEntity} from "../../entities";
+import {ModalDeleteComponent} from "@minhdu-fontend/components";
 
 @Component({
-  selector:'minhdu-fontend-rank',
+  selector: 'minhdu-fontend-setting-salary',
   templateUrl: 'setting-salary.component.html'
 })
-export class SettingSalaryComponent implements OnInit{
-  ranks$ = new Observable<any>()
-  positions$ = new Observable<Position[]>()
-  branches$ =  new Observable<Branch[]>()
-  formGroup = new FormGroup({
-    search: new FormControl(''),
-    name: new FormControl(''),
-    gender: new FormControl(''),
-    position: new FormControl([]),
-    branch: new FormControl('')
-  });
-  pageSize = 10
+export class SettingSalaryComponent implements OnInit {
+  settingSalaries$ = this.settingSalaryQuery.selectAll();
+  loading$ = this.settingSalaryQuery.selectLoading();
+  stateSearch = this.settingSalaryQuery.getValue().search;
+  formGroup = new FormGroup(
+    {
+      search: new FormControl(''),
+    }
+  );
+  panelOpenState = false;
+  pageSizeTable = 10;
+  visible = false;
+  pageSize = 10;
+  salaryTypeEnum = SalaryTypeEnum;
+  blockSalaries = blockSalariesConstant;
+  compareFN = (o1: any, o2: any) => (o1 && o2 ? o1.id == o2.id : o1 === o2);
 
   constructor(
+    private readonly settingSalaryQuery: SettingSalaryQuery,
+    private readonly actions$: Actions,
+    private readonly dialog: MatDialog,
+    private readonly modal: NzModalService,
+    private readonly settingSalaryStore: SettingSalaryStore
   ) {
   }
 
   ngOnInit() {
-    this.formGroup.valueChanges.subscribe(val => {
-      //search
-    })
+    this.actions$.dispatch(SettingSalaryActions.loadAll({
+      search: this.mapProduct(this.formGroup.value, false)
+    }));
+    this.formGroup.valueChanges.pipe(
+      debounceTime(1000),
+      map(value => {
+          this.actions$.dispatch(SettingSalaryActions.loadAll({
+            search: this.mapProduct(this.formGroup.value, false)
+          }));
+        }
+      )
+    ).subscribe();
   }
 
   onPagination(index: number) {
-    //pagination
+    const count = this.settingSalaryQuery.getCount();
+    if (index * this.pageSizeTable >= count) {
+      this.actions$.dispatch(SettingSalaryActions.loadAll({
+        search: this.mapProduct(this.formGroup.value, true),
+        isPaginate: true
+      }));
+    }
+  }
+
+  mapProduct(dataFG: any, isPagination: boolean) {
+    this.settingSalaryStore.update(state => ({
+      ...state, search: dataFG
+    }));
+    Object.assign(dataFG, {
+      take: PaginationDto.take,
+      skip: isPagination ? this.settingSalaryQuery.getCount() : PaginationDto.skip
+    });
+    return dataFG;
+  }
+
+  tranFormType(salaryTypes: SalaryTypeEnum[]) {
+    return salaryTypes.map(val => {
+      return  this.blockSalaries.find((item: any) => item.type === val)?.title
+    }).join(' + ')
+  }
+
+  onAdd(template?: SettingSalaryEntity) {
+    this.modal.create({
+      nzWidth:'fit-content',
+      nzTitle: 'Tạo bản mẫu lương',
+      nzContent: SettingSalaryDialogComponent,
+      nzComponentParams: {
+        data: {template: template}
+      },
+      nzFooter: null,
+    })
+  }
+
+  onUpdate(template: SettingSalaryEntity) {
+    this.modal.create({
+      nzWidth:'fit-content',
+      nzTitle: 'Tạo bản mẫu lương',
+      nzContent: SettingSalaryDialogComponent,
+      nzComponentParams:{
+        data: {isUpdate: true , template : template}
+      },
+      nzFooter: null,
+    })
+  }
+
+  onDelete(template: SettingSalaryEntity) {
+    this.modal.create({
+      nzWidth:'fit-content',
+      nzTitle: 'Xoá bản mẫu lương',
+      nzContent: ModalDeleteComponent,
+      nzComponentParams:{
+        data: {description: `bản mẫu lương ${template.title}`}
+      },
+      nzFooter: null,
+    }).afterClose.subscribe(val => {
+      if(val){
+        this.actions$.dispatch(SettingSalaryActions.remove({
+          id: template.id
+        }))
+      }
+    })
   }
 }
--- apps/hrv2/src/app/pages/setting/setting-routing.module.ts
+++ apps/hrv2/src/app/pages/setting/setting-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {SettingSalaryComponent} from "./salary/container";
+import {SettingSalaryComponent} from "./salary/containers/setting-salary/setting-salary.component";
 
 const routes: Routes = [
   {
