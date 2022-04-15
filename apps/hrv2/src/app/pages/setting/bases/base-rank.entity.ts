440c7946b3c42565623678048eea7d9699646f89
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-15 10:27:25 +0700

    s

--- apps/hrv2/src/app/pages/setting/bases/base-rank.entity.ts
+++
@@ -1,8 +0,0 @@
-import {BaseEntity} from "@minhdu-fontend/base-entity";
-import {Employee} from "@minhdu-fontend/data-models";
-
-export interface BaseRankEntity extends BaseEntity {
-  employee:Employee,
-  dayOff:number,
-  salary: number
-}
