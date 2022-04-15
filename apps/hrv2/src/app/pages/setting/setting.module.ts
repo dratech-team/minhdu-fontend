e6ff9d834351594df633ca32aa528e564f0ce166
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-15 10:26:28 +0700

    s

---
+++ apps/hrv2/src/app/pages/setting/setting.module.ts
@@ -0,0 +1,48 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {SettingRoutingModule} from "./setting-routing.module";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ComponentsModule} from "@minhdu-fontend/components";
+import {NzDropDownModule} from "ng-zorro-antd/dropdown";
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {SharedModule} from "../../../shared/shared.module";
+import {SettingSalaryComponent} from "./salary/container";
+import {NzSelectModule} from "ng-zorro-antd/select";
+import {NzPopoverModule} from "ng-zorro-antd/popover";
+import {TransformBlockSalaryPipe} from "./salary/pipes";
+import {SettingSalaryDialogComponent, VisibleSettingSalaryComponent} from "./salary/components";
+import {NzRadioModule} from "ng-zorro-antd/radio";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NgxCurrencyModule} from "ngx-currency";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CommonModule,
+    SettingRoutingModule,
+    NzTableModule,
+    NzCollapseModule,
+    ReactiveFormsModule,
+    ComponentsModule,
+    NzDropDownModule,
+    NzIconModule,
+    NzSelectModule,
+    NzPopoverModule,
+    NzRadioModule,
+    NzCheckboxModule,
+    NgxCurrencyModule,
+  ],
+  declarations:[
+    SettingSalaryComponent,
+    TransformBlockSalaryPipe,
+    SettingSalaryDialogComponent,
+    VisibleSettingSalaryComponent
+  ],
+})
+
+export class SettingModule {
+
+}
