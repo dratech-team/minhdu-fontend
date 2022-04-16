f7cddd532103c84e9c19e53fcd264700201f38df
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-04-17 01:29:15 +0700

    a

--- apps/hr/src/app/pages/payroll/constants/session.constant.ts
+++ apps/hr/src/app/pages/payroll/constants/session.constant.ts
@@ -1,8 +1,9 @@
-import {PartialDayEnum} from "@minhdu-fontend/data-models";
+import { PartialDayEnum } from '@minhdu-fontend/data-models';
 
 export const SessionConstant = [
   {
     name: 'Buổi sáng',
+    detail: 'Ngày buổi sáng',
     value: PartialDayEnum.MORNING,
     startTime: new Date(0, 0, 0, 7, 0),
     endTime: new Date(0,
@@ -10,18 +11,26 @@ export const SessionConstant = [
   },
   {
     name: 'Buổi chiều',
+    detail: 'Ngày buổi chiều',
     value: PartialDayEnum.AFTERNOON,
     startTime: new Date(0, 0, 0, 13, 30),
     endTime: new Date(0, 0, 0, 17, 0)
   },
   {
     name: 'Nguyên ngày',
+    detail: 'Ngày',
     value: PartialDayEnum.ALL_DAY,
     startTime: new Date(0, 0, 0, 7, 0),
     endTime: new Date(0, 0, 0, 17, 0)
   },
-  {name: 'Tuỳ chọn', value: PartialDayEnum.CUSTOM},
+  {
+    name: 'Tuỳ chọn',
+    detail: 'Phút',
+    value: PartialDayEnum.CUSTOM,
+    startTime: new Date(0, 0, 0, 7, 0),
+    endTime: new Date(0, 0, 0, 17, 0)
+  }
 ];
 
 
-export const DisableTimeConstant = [0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 22, 23, 24]
+export const DisableTimeConstant = [0, 1, 2, 3, 4, 5, 6, 7, 17, 18, 19, 20, 21, 22, 23, 24];
