5613a45594f207647ef14c7360966bbc457571f4
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-16 09:30:58 +0700

    s

--- apps/hr/src/app/pages/template/component/template-salary/template-salary.component.ts
+++ apps/hr/src/app/pages/template/component/template-salary/template-salary.component.ts
@@ -2,7 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {select, Store} from '@ngrx/store';
-import {SalaryTypeEnum} from '@minhdu-fontend/enums';
+import {DatetimeUnitEnum, SalaryTypeEnum} from '@minhdu-fontend/enums';
 import {TemplateSalaryAction} from '../../+state/teamlate-salary/template-salary.action';
 import {selectTemplateAdded} from '../../+state/teamlate-salary/template-salary.selector';
 import {getAllOrgchart, OrgchartActions} from '@minhdu-fontend/orgchart';
@@ -11,8 +11,9 @@ import * as lodash from 'lodash';
 import {NzMessageService} from 'ng-zorro-antd/message';
 import {DiveTypeEnum, salaryReference} from "../../enums";
 import {blockSalariesConstant} from "../../constants";
-import {SalaryConstraint, SalarySetting} from "../../+state/teamlate-salary/salary-setting";
-import {diveConstant, referencesTypeConstant} from "../../constants/references-type.constant";
+import {SalarySetting} from "../../+state/teamlate-salary/salary-setting";
+import {recipesConstant, referencesTypeConstant} from "../../constants/references-type.constant";
+import {UnitSalaryConstant} from "../../constants/unit-salary.constant";
 
 @Component({
   templateUrl: 'template-salary.component.html'
@@ -23,6 +24,7 @@ export class TemplateSalaryComponent implements OnInit {
   formGroup!: FormGroup;
   submitted = false;
   blockSalary = blockSalariesConstant;
+  unitConstant = UnitSalaryConstant
   salaryTypeEnum = SalaryTypeEnum
   branches = new FormControl();
   branchesSelected: Branch[] = [];
@@ -40,14 +42,15 @@ export class TemplateSalaryComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(OrgchartActions.init());
-
+    console.log(this.data.template)
     const template = this.data?.template
     this.formGroup = this.formBuilder.group({
       block: [template?.type === SalaryTypeEnum.BASIC_INSURANCE ? this.blockSalary.find(block => block.type === SalaryTypeEnum.BASIC) :
         this.blockSalary.find(block => block.type === template?.type)
         , Validators.required],
       price: [template?.price],
-      recipes: [],
+      unit: [template?.unit || DatetimeUnitEnum.MONTH],
+      recipes: [template?.types ? this.getRecipes(template.types) : ''],
       reference: [template?.types?.length > 0 ? this.getReference(salaryReference.BLOCK) : this.getReference(salaryReference.PRICE)],
       title: [template?.title, Validators.required],
       dive: [template?.workday ? DiveTypeEnum.OTHER : DiveTypeEnum.STANDARD],
@@ -71,13 +74,16 @@ export class TemplateSalaryComponent implements OnInit {
     if (!constraints) {
       return false
     } else {
-      console.log(constraints)
       return constraints.some(val => val === constraint)
     }
   }
 
   getReference(salaryReference: salaryReference) {
     return referencesTypeConstant.find(referenceConstant => referenceConstant.value === salaryReference)
+  }
+
+  getRecipes(types: SalaryTypeEnum[]) {
+    return recipesConstant.filter(value => types.includes(value.value))
   }
 
   get checkValid() {
@@ -98,6 +104,7 @@ export class TemplateSalaryComponent implements OnInit {
     }
     const template = {
       title: value.title,
+      unit: value.unit,
       settingType: value.block.type === SalaryTypeEnum.BASIC && value.insurance ?
         SalaryTypeEnum.BASIC_INSURANCE :
         value.block.type,
---
+++ apps/hr/src/app/pages/template/constants/unit-salary.constant.ts
@@ -0,0 +1,12 @@
+import {DatetimeUnitEnum} from "@minhdu-fontend/enums";
+
+export const UnitSalaryConstant = [
+  {
+    name: 'Ngày',
+    value: DatetimeUnitEnum.DAY
+  },
+  {
+    name: 'Tháng',
+    value: DatetimeUnitEnum.MONTH
+  }
+];
