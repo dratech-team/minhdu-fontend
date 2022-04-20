83480133fb519b0cdd54b09a05582ebe85565e4b
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-04-20 15:40:58 +0700

    s

--- apps/hrv2/src/app/pages/salary/salary.module.ts
+++ apps/hrv2/src/app/pages/salary/salary.module.ts
@@ -1,17 +1,9 @@
 import {NgModule} from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
 import {CommonModule, DatePipe} from '@angular/common';
-import {MatCheckboxModule} from '@angular/material/checkbox';
 import {HttpClientModule} from '@angular/common/http';
-import {MatDialogModule} from '@angular/material/dialog';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatStepperModule} from '@angular/material/stepper';
 import {AkitaNgEffectsModule} from '@datorama/akita-ng-effects';
 import {NzMessageModule} from 'ng-zorro-antd/message';
-import {MatSortModule} from '@angular/material/sort';
 import {NzModalModule} from 'ng-zorro-antd/modal';
 import {NzCollapseModule} from 'ng-zorro-antd/collapse';
 import {NzRadioModule} from 'ng-zorro-antd/radio';
