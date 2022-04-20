dec8fbfd80ebc1800d2bc6a295a42661bb5cda62
Trần Hữu Long <48413548+uranussolutions@users.noreply.github.com>
2022-04-20 19:25:04 +0700

    Update update-absent-salary.dto.ts

--- apps/hrv2/src/app/pages/salary/dto/absent-salary/update-absent-salary.dto.ts
+++ apps/hrv2/src/app/pages/salary/dto/absent-salary/update-absent-salary.dto.ts
@@ -4,7 +4,7 @@ import { BaseUpdateDto } from '@minhdu-fontend/base-dto';
 export interface BaseUpdateAbsentDto extends BaseSalaryEntity {
   readonly settingId: number,
   readonly startedAt: Date,
-  readonly EndedAt: Date
+  readonly endedAt: Date
 }
 
 export type UpdateAbsentSalaryDto = BaseUpdateDto<BaseUpdateAbsentDto>
