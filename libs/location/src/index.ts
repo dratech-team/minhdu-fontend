5e4402791eb9bb33dac5b59b9db042640ddee0a8
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-05-02 20:20:42 +0700

    oprimize code

--- libs/location/src/index.ts
+++ libs/location/src/index.ts
@@ -3,6 +3,9 @@ export * from './lib/+state/province/nation.action';
 export * from './lib/+state/province/province.selector';
 export * from './lib/+state/district/district.selector';
 export * from './lib/+state/ward/ward.selector';
+export * from './lib/service/nation.service';
 export * from './lib/service/province.service';
+export * from './lib/service/district.service';
+export * from './lib/service/ward.service';
 
 
