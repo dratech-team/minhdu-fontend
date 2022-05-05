a359123f0a28fdff8561073719278b4c431899dd
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-05 09:42:19 +0700

    pipe total price

---
+++ libs/components/src/lib/pipes/total-price.pipe.ts
@@ -0,0 +1,11 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'totalprice',
+  pure: false,
+})
+export class TotalPricePipe implements PipeTransform {
+  transform(arr: any[] | readonly any []): number {
+    return arr.reduce((a, b) => a + b.price, 0)
+  }
+}
