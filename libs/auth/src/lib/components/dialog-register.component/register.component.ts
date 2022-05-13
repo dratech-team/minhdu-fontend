101dda94837efdaec87dbdd778767580f5254998
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-13 15:25:29 +0700

    fix resignter account

--- libs/auth/src/lib/components/dialog-register.component/register.component.ts
+++ libs/auth/src/lib/components/dialog-register.component/register.component.ts
@@ -1,53 +1,55 @@
-import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { select, Store } from '@ngrx/store';
-import { Localhost } from '../../../../../enums/localhost.enum';
-import { App } from '@minhdu-fontend/enums';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { AuthActions } from '@minhdu-fontend/auth';
-import { Branch } from '@minhdu-fontend/data-models';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { getAllOrgchart, OrgchartActions } from '@minhdu-fontend/orgchart';
-import { combineLatest, Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import * as lodash from 'lodash';
-import { RoleService } from '../../services/role.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {App} from '@minhdu-fontend/enums';
+import {RoleService} from '../../services/role.service';
+import {BranchActions, BranchEntity, BranchQuery} from "@minhdu-fontend/orgchart-v2";
+import {Actions} from "@datorama/akita-ng-effects";
+import {AccountEntity} from "../../../../../system/src/lib/entities/account.entity";
+import {NzModalRef} from "ng-zorro-antd/modal";
+import {AccountActions} from "../../../../../system/src/lib/state/account-management/account.actions";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {Store} from "@ngrx/store";
+import {BaseUpdateAccount} from "../../../../../system/src/lib/dto/account/update-account.dto";
+import {AddAccountDto, BaseAddAccountDto} from "../../../../../system/src/lib/dto/account/add-account.dto";
+import {AccountQuery} from "../../../../../system/src/lib/state/account-management/account.query";
 
 @Component({
   templateUrl: 'register.component.html'
 })
 export class RegisterComponent implements OnInit {
-  @ViewChild('branchInput') branchInput!: ElementRef;
+  @Input() data?: {
+    update: {
+      account: AccountEntity
+    }
+  }
+  branches$ = this.branchQuery.selectAll()
+  role$ = this.roleService.getAll();
+  added$ = this.accountQuery.select(state => state.added)
+
   localhost = `${window.location.host}`;
   app = App;
-  isHidden = false;
   formGroup!: FormGroup;
-  branchesSelected: Branch[] = [];
-  branches = new FormControl();
-  branches$ = this.store.pipe(select(getAllOrgchart));
-  submitted = false;
-  role$!: Observable<any[]>;
-  appName!: string;
+  compareFN = (o1: any, o2: any) => (o1 && o2 ? o1.id == o2.id : o1 === o2);
 
   constructor(
     private readonly formBuilder: FormBuilder,
-    private readonly store: Store,
-    private readonly snackbar: MatSnackBar,
+    private readonly branchQuery: BranchQuery,
+    private readonly accountQuery: AccountQuery,
     private readonly roleService: RoleService,
-    private dialogRef: MatDialogRef<RegisterComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    private readonly actions$: Actions,
+    private readonly modalRef: NzModalRef,
+    private readonly message: NzMessageService
   ) {
   }
 
 
   ngOnInit() {
-    this.role$ = this.roleService.getAll();
-    this.store.dispatch(OrgchartActions.init());
-    if (this.data?.isUpdate) {
-      this.branchesSelected = [...this.data.account?.branches];
+    this.actions$.dispatch(BranchActions.loadAll({}))
+    if (this.data?.update) {
       this.formGroup = this.formBuilder.group(
         {
-          role: [this.data.account.role, Validators.required]
+          role: [this.data.update.account.role, Validators.required],
+          branches: [this.data.update.account.branches],
         }
       );
     } else {
@@ -56,30 +58,11 @@ export class RegisterComponent implements OnInit {
           userName: ['', Validators.required],
           password: ['', Validators.required],
           password2: ['', Validators.required],
-          role: ['', Validators.required]
-          // role2: [],
-          // role3: [],
-          // role4: [],
-          // role5: []
+          role: ['', Validators.required],
+          branches: [[]],
         }
       );
     }
-
-
-    this.branches$ = combineLatest([
-      this.branches.valueChanges.pipe(startWith('')),
-      this.branches$
-    ]).pipe(
-      map(([branch, branches]) => {
-        if (branch) {
-          return branches.filter((e) => {
-            return e.name.toLowerCase().includes(branch?.toLowerCase());
-          });
-        } else {
-          return branches;
-        }
-      })
-    );
   }
 
   get checkValid() {
@@ -87,58 +70,37 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): any {
-    this.submitted = true;
     if (this.formGroup.invalid) {
       return;
     }
-    if (this.branchesSelected.length === 0) {
-      return this.snackbar.open('Chưa chọn đơn vị', '', { duration: 1500 });
-    }
     const val = this.formGroup.value;
-    const account = {
+    if (!this.data?.update && val.password !== val.password2) {
+      return this.message.warning('Nhập lại mật khẩu chưa đúng')
+    }
+    const account = this.mapAccount(val)
+    this.data?.update
+      ? this.actions$.dispatch(AccountActions.update({
+        id: this.data.update.account.id,
+        updates: account
+      }))
+      : this.actions$.dispatch(AccountActions.addOne({
+          body: account
+        })
+      )
+    this.added$.subscribe(val => {
+      if(val){
+        this.modalRef.close();
+      }
+    })
+  }
+
+  mapAccount(val: any): BaseAddAccountDto {
+    return {
       username: val.userName,
       password: val.password,
-      branchIds: this.branchesSelected.map(item => {
-        return item.id;
-      }),
+      branchIds: val.branches.map((item: BranchEntity) => item.id),
       roleId: val.role.id,
-      appName: val.role.appName
+      appName: val.role.appName,
     };
-    if (this.data?.isUpdate) {
-      this.store.dispatch(AuthActions.updateAccount(
-        {
-          id: this.data.account.id,
-          branchIds: this.branchesSelected.map(item => {
-            return item.id;
-          }),
-          roleId: val.role.id
-        }));
-    } else {
-      if (val.password2 === val.password) {
-        this.store.dispatch(AuthActions.signUp({ accountDTO: account }));
-      } else {
-        this.isHidden = true;
-      }
-    }
-    this.dialogRef.close();
-  }
-
-  onSelectBranch(event: any, branch: Branch, branchInput: HTMLElement) {
-    if (event.isUserInput) {
-      if (this.branchesSelected.find(item => item.id === branch.id)) {
-        this.snackbar.open('Đơn vị đã được chọn', '', { duration: 1500 });
-      } else {
-        this.branchesSelected.push(branch);
-      }
-      setTimeout(() => {
-          this.branches.setValue('');
-          branchInput.blur();
-        }
-      );
-    }
-  }
-
-  removeBranches(branch: Branch) {
-    lodash.remove(this.branchesSelected, branch);
   }
 }
