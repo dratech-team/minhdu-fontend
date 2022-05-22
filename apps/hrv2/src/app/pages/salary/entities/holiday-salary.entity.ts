0953c495b2352e52f16893af20f51c164522e44b
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-22 22:38:28 +0700

    inertable holiday (#1023)
    * inertable holiday
    
    * Update payroll.effect.ts

--- apps/hrv2/src/app/pages/salary/entities/holiday-salary.entity.ts
+++ apps/hrv2/src/app/pages/salary/entities/holiday-salary.entity.ts
@@ -2,11 +2,13 @@ import {PayrollEntity} from "../../payroll/entities";
 import {SalarySettingEntity} from "../../setting/salary/entities";
 import {BaseEntity} from "@minhdu-fontend/base-entity";
 
-export interface HolidaySalaryEntity extends BaseEntity{
+export interface HolidaySalaryEntity extends BaseEntity {
   readonly payroll?: PayrollEntity,
   readonly payrollId: number
   readonly setting: SalarySettingEntity
   readonly duration: number
   readonly note?: string
-  readonly total: number
+  readonly total: number;
+  readonly details: HolidaySalaryEntity & { price: number }[]
+  expand: boolean
 }
