6b3556041e2792bdd8c6f86c3d0336d90961d5bb
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-24 23:04:13 +0700

    Update filter-day.pipe.ts

--- libs/components/src/lib/pipes/filter-day.pipe.ts
+++ libs/components/src/lib/pipes/filter-day.pipe.ts
@@ -1,13 +1,18 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {sortDatetime} from '../../../../utils/daytime.until';
+import {sortDatetime} from '@minhdu-fontend/utils';
 
 @Pipe({
   name: 'filterDay',
   pure: false,
 })
 export class FilterDayPipe implements PipeTransform {
-  transform(arr: (readonly any[]) | any []) {
-    const arrResult = JSON.parse(JSON.stringify(arr))
-    return sortDatetime(arrResult)
+  transform(arr: (readonly any[]) | any [] | undefined) {
+    if (arr){
+      const arrResult = JSON.parse(JSON.stringify(arr))
+      return sortDatetime(arrResult)
+    }else{
+      return []
+    }
+
   }
 }
