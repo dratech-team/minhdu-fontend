ffa5ba6ceab74de058b01c354aa8d9f77e25f904
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-24 09:48:57 +0700

    fix allowance

--- apps/hrv2/src/app/pages/setting/salary/constants/unit-datetime.constant.ts
+++ apps/hrv2/src/app/pages/setting/salary/constants/unit-datetime.constant.ts
@@ -2,15 +2,15 @@ import {DatetimeUnitEnum} from '@minhdu-fontend/enums';
 import {SalaryTypeEnum} from '../enums';
 
 export const UnitDatetimeConstant: { name: string, value: DatetimeUnitEnum, salaryType: SalaryTypeEnum[] }[] = [
-  // {
-  //   name:'Tháng',
-  //   value: DatetimeUnitEnum.MONTH,
-  //   salaryType: [SalaryTypeEnum.ABSENT]
-  // },
+  {
+    name:'Tháng',
+    value: DatetimeUnitEnum.MONTH,
+    salaryType: [SalaryTypeEnum.ALLOWANCE]
+  },
   {
     name: 'Ngày',
     value: DatetimeUnitEnum.DAY,
-    salaryType: [SalaryTypeEnum.ABSENT, SalaryTypeEnum.OVERTIME, SalaryTypeEnum.HOLIDAY]
+    salaryType: [SalaryTypeEnum.ABSENT, SalaryTypeEnum.OVERTIME, SalaryTypeEnum.HOLIDAY ,SalaryTypeEnum.ALLOWANCE]
   },
   {
     name: 'Giờ',
