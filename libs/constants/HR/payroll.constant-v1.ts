e7f6bd71c799d05b32b70dd329bc034edc707c57
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-25 13:33:57 +0700

    fix constantpayroll  v1

---
+++ libs/constants/HR/payroll.constant-v1.ts
@@ -0,0 +1,37 @@
+import { FilterTypeEnum } from '../../enums';
+
+
+export const PayrollConstantV1 = [
+  {
+    name: 'Bảng lương',
+    value: FilterTypeEnum.PAYROLL,
+  },
+  {
+    name: 'Bảng tăng ca',
+    value: FilterTypeEnum.OVERTIME,
+  },
+  {
+    name: 'Bảng chấm công',
+    value: FilterTypeEnum.TIME_SHEET,
+  },
+  {
+    name: 'Bảng lương công nhật',
+    value: FilterTypeEnum.SEASONAL,
+  },
+  {
+    name: 'Bảng lương cơ bản',
+    value: FilterTypeEnum.BASIC
+  },
+  {
+    name: 'Bảng phụ cấp',
+    value: FilterTypeEnum.STAY
+  },
+  {
+    name: 'Bảng phụ cấp khác',
+    value: FilterTypeEnum.ALLOWANCE
+  },
+  {
+    name: 'Bảng khấu trừ',
+    value: FilterTypeEnum.ABSENT
+  },
+]
