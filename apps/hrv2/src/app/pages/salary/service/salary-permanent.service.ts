34df63df111cc59e3b4a5f99e884b9519f87510d
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-25 13:55:05 +0700

    fix constant salaryv2

--- apps/hrv2/src/app/pages/salary/service/salary-permanent.service.ts
+++ apps/hrv2/src/app/pages/salary/service/salary-permanent.service.ts
@@ -5,20 +5,19 @@ import {Api} from '@minhdu-fontend/constants';
 import {Observable} from 'rxjs';
 import {ResponseMessageEntity} from "@minhdu-fontend/base-entity";
 import {PermanentSalaryEntity} from "../entities";
+import {VersionEnum} from "@minhdu-fontend/enums";
 
 @Injectable({providedIn: 'root'})
 export class SalaryPermanentService extends BaseService<PermanentSalaryEntity> {
   constructor(
     public readonly http: HttpClient
   ) {
-    super(Api.HR.PAYROLL.SALARY, http);
+    super(Api.HR.PAYROLL.SALARY_V2, http,VersionEnum.V2);
   }
 
   addMany(body: any): Observable<ResponseMessageEntity> {
     return super.addMany(body);
   }
-
-
 
   updateMany(body: any): Observable<ResponseMessageEntity> {
     return super.updateMany(body);
