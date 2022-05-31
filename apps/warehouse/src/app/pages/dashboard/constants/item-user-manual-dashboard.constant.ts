547ffe504c2b7da39bc36241ba46b7ab065ccb47
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-05-31 21:11:01 +0700

    s (#1033)

---
+++ apps/warehouse/src/app/pages/dashboard/constants/item-user-manual-dashboard.constant.ts
@@ -0,0 +1,27 @@
+export const ItemUserManualDashboardConstant = [
+  {
+    title: 'Bước 1 : Thêm sản phẩm',
+    description: 'Tạo mới và quản lý tồn kho theo từng phiên bản sản phẩm',
+    readingTime: 1,
+  },
+  {
+    title: 'Bước 1 : Thêm sản phẩm',
+    description: 'Tạo mới và quản lý tồn kho theo từng phiên bản sản phẩm',
+    readingTime: 1,
+  },
+  {
+    title: 'Bước 1 : Thêm sản phẩm',
+    description: 'Tạo mới và quản lý tồn kho theo từng phiên bản sản phẩm',
+    readingTime: 1,
+  },
+  {
+    title: 'Bước 1 : Thêm sản phẩm',
+    description: 'Tạo mới và quản lý tồn kho theo từng phiên bản sản phẩm',
+    readingTime: 1,
+  },
+  {
+    title: 'Bước 1 : Thêm sản phẩm',
+    description: 'Tạo mới và quản lý tồn kho theo từng phiên bản sản phẩm',
+    readingTime: 1,
+  },
+]
