7d8adf18e25f1c21fab54476abf51d3df61822c4
giahieu123456 <66545389+giahieu123456@users.noreply.github.com>
2022-06-21 23:32:58 +0700

    cmment

--- apps/hrv2/src/app/pages/payroll/dto/load-one-payroll.dto.ts
+++ apps/hrv2/src/app/pages/payroll/dto/load-one-payroll.dto.ts
@@ -2,6 +2,9 @@ import {BaseLoadOneDto} from "@minhdu-fontend/base-dto";
 import {SalaryTypeEnum} from "@minhdu-fontend/enums";
 
 export interface LoadOnePayrollDto extends BaseLoadOneDto {
+  // khi update thành công một salary bất kỳ hiện tại bắt buộc phải gọi lại get one( logic tính toán),
+  // hiện tại đang truyền vào salary type và salaryId vừa mới dc update thành công, để chỉ update date lại salary đó trong store
+  // để không làm thay đổi thứ tự các salary (thứ tự sau khi  sort)
   updateOneSalary?: {
     salaryType: SalaryTypeEnum
     salaryId: number
