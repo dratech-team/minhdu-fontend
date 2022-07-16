a3a79f0baf053031110b4d2eb049b4fbef0b9002
Long Tran <tranlong.kma@gmail.com>
2022-07-16 16:11:04 +0700

    no message

--- apps/sell/src/app/pages/order/state/order.query.ts
+++ apps/sell/src/app/pages/order/state/order.query.ts
@@ -1,8 +1,13 @@
-import { QueryEntity } from '@datorama/akita';
+import { Order, QueryConfig, QueryEntity } from '@datorama/akita';
 import { OrderState, OrderStore } from './order.store';
 import { Injectable } from '@angular/core';
+import { OrderEntity } from '../enitities/order.entity';
 
 @Injectable({ providedIn: 'root' })
+@QueryConfig<OrderEntity>({
+  sortBy: "createdAt",
+  sortByOrder: Order.DESC,
+})
 export class OrderQuery extends QueryEntity<OrderState> {
   constructor(protected store: OrderStore) {
     super(store);
