7d0bf4d4eb0d46137f317ce71f84f331b925f728
Long Tran <tranlong.kma@gmail.com>
2022-07-25 11:42:14 +0700

    optimize: overview

--- apps/sell/src/app/pages/statistical/component/pick-statistical-type/pick-statistical-type.component.ts
+++ apps/sell/src/app/pages/statistical/component/pick-statistical-type/pick-statistical-type.component.ts
@@ -1,47 +1,58 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {
-  UntypedFormBuilder,
-  UntypedFormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FilterOverviewEnum, OptionOverviewEnum } from '@minhdu-fontend/enums';
+import * as dateFns from 'date-fns';
 
 @Component({
   selector: 'app-pick-statical',
-  templateUrl: 'pick-statistical-type.component.html',
+  templateUrl: 'pick-statistical-type.component.html'
 })
 export class PickStatisticalTypeComponent implements OnInit {
   @Input() type!: FilterOverviewEnum;
-  @Output() EventPrint = new EventEmitter<any>();
-  @Output() EventStatistical = new EventEmitter<any>();
+  @Output() onExport = new EventEmitter<any>();
+  @Output() onChangeValue = new EventEmitter<any>();
+
+  thisMonth = [dateFns.startOfMonth(new Date()), dateFns.endOfMonth(new Date())];
+
+  ranges = {
+    'Hôm nay': [new Date(), new Date()],
+    'Tháng này': this.thisMonth,
+    '3 Tháng gần nhất': [dateFns.startOfMonth(dateFns.subMonths(new Date(), 3)), dateFns.endOfMonth(new Date())],
+    'Năm nay': [dateFns.startOfYear(new Date()), dateFns.endOfYear(new Date())],
+    'Năm trước': [dateFns.startOfYear(dateFns.subYears(new Date(), 1)), dateFns.endOfYear(new Date())]
+  };
 
   optionOverview = OptionOverviewEnum;
   filterOverview = FilterOverviewEnum;
-  formGroup!: UntypedFormGroup;
 
-  constructor(private readonly formBuilder: UntypedFormBuilder) {}
+  formGroup = new FormGroup({
+    option: new FormControl<OptionOverviewEnum>(OptionOverviewEnum.SALES, { validators: Validators.required }),
+    range: new FormControl<Date[]>(this.thisMonth, { validators: Validators.required })
+  });
 
   ngOnInit() {
-    this.formGroup = this.formBuilder.group({
-      option: [this.optionOverview.SALES, Validators.required],
-      startedAt: [undefined, Validators.required],
-      endedAt: [undefined, Validators.required],
-    });
-    this.formGroup.valueChanges.subscribe((val) => {
-      if ((val.startedAt && val.endedAt) || (!val.startedAt && !val.endedAt)) {
-        this.EventStatistical.emit(val);
+    this.formGroup.valueChanges.subscribe((value) => {
+      if (value.range?.length === 0) {
+        this.formGroup.controls.range.setValue(this.thisMonth);
+      }
+      if ((value.range?.length === 2 && value.range[0] && value.range[1])) {
+        this.onChangeValue.emit({
+          option: value.option,
+          startedAt: new Date(value.range[0]),
+          endedAt: new Date(value.range[1])
+        });
       }
     });
   }
 
-  print() {
-    const val = this.formGroup.value;
-    const params = {
-      startedAt: val.startedAt,
-      endedAt: val.endedAt,
-      option: val.option,
-      print: true,
-    };
-    this.EventPrint.emit(params);
-  }
+  // print() {
+  //   const val = this.formGroup.value;
+  //   const params = {
+  //     startedAt: val.startedAt,
+  //     endedAt: val.endedAt,
+  //     option: val.option,
+  //     print: true
+  //   };
+  //   this.onExport.emit(params);
+  // }
 }
--- apps/sell/src/app/pages/statistical/statistical.module.ts
+++ apps/sell/src/app/pages/statistical/statistical.module.ts
@@ -4,15 +4,12 @@ import { StatisticalRoutingModule } from './statistical-routing.module';
 import { CommonModule, DatePipe } from '@angular/common';
 import { BarChartModule } from '@swimlane/ngx-charts';
 import { ComponentsModule } from '@minhdu-fontend/components';
-import { MatSelectModule } from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PickStatisticalTypeComponent } from './component/pick-statistical-type/pick-statistical-type.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { PickDayToDayComponent } from './component/pick-day-to-day/pick-day-to-day.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
 import { StatisticalService } from './service/statistical/statistical.service';
 import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @NgModule({
   imports: [
@@ -20,20 +17,18 @@ import { NzMessageModule } from 'ng-zorro-antd/message';
     StatisticalRoutingModule,
     CommonModule,
     BarChartModule,
-    MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
-    MatDialogModule,
-    MatMenuModule,
-    MatButtonModule,
     NzMessageModule,
+    NzDatePickerModule,
+    NzSelectModule
   ],
   declarations: [
-    PickDayToDayComponent,
     StatisticalComponent,
-    PickStatisticalTypeComponent,
+    PickStatisticalTypeComponent
   ],
   exports: [StatisticalComponent],
-  providers: [StatisticalService, DatePipe],
+  providers: [StatisticalService, DatePipe]
 })
-export class StatisticalModule {}
+export class StatisticalModule {
+}
