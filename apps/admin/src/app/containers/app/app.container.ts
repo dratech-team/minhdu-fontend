586bea9d59a25c516cc745bfc802b15c45cff661
Long Tran <tranlong.kma@gmail.com>
2022-07-31 23:21:30 +0700

    no message

--- apps/admin/src/app/containers/app/app.container.ts
+++ apps/admin/src/app/containers/app/app.container.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppService } from './app.service';
-import { Observable } from 'rxjs';
-import { AppInterface } from './app.interface';
 
 @Component({
   templateUrl: 'app.container.html',
-  styleUrls: ['app.container.scss'],
+  styleUrls: ['app.container.scss']
 })
-export class AppContainer implements OnInit {
-  appMinhDuConstant$!: Observable<AppInterface[]>;
+export class AppContainer {
+  apps$ = this.appService.getAll();
 
   constructor(
     private readonly router: Router,
     private readonly appService: AppService
-  ) {}
-
-  ngOnInit() {
-    this.appMinhDuConstant$ = this.appService.getAll();
+  ) {
   }
 }
