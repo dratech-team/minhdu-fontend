8fc11728d63ca6f4b05fbcf9c8204c9ae835297c
Long Tran <tranlong.kma@gmail.com>
2022-08-02 19:56:13 +0700

    no message

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -2,16 +2,10 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { persistState } from '@datorama/akita';
-import { StorageName } from '@minhdu-fontend/constants';
 
 if (environment.production) {
   enableProdMode();
 }
-
-persistState({
-  include: [StorageName.ACCOUNT]
-});
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
